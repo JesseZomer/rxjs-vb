fa534d7869472005fcf73efef421a9c190405152
Jesse Zomer <jessezomer@hotmail.com>
2024-01-16 12:55:59 +0100

    calendar-day met signal inputs

--- src/app/calendar/calendar-day/calendar-day.component.ts
+++ src/app/calendar/calendar-day/calendar-day.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, computed, input } from '@angular/core';
 import { isSameDay } from 'date-fns';
 import { Afspraak } from '../../afspraken.service';
 
@@ -11,23 +11,15 @@ import { Afspraak } from '../../afspraken.service';
     styleUrl: './calendar-day.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CalendarDayComponent implements OnChanges {
-    @Input({ required: true }) dag: Date;
-    @Input({ required: true }) peildatum: Date;
-    @Input({ required: true }) afspraken: Afspraak[];
-    @Input({ required: true }) selectedAfspraakId: string | undefined | null;
+export class CalendarDayComponent {
+    dag = input.required<Date>();
+    peildatum = input.required<Date>();
+    afspraken = input.required<Afspraak[]>();
+    selectedAfspraakId = input.required<string | undefined | null>();
+
+    dagnummer = computed(() => this.dag().getDate());
+    isToday = computed(() => isSameDay(this.dag(), new Date()));
+    isPeildatum = computed(() => isSameDay(this.dag(), this.peildatum()));
 
     @Output() selectAfspraak = new EventEmitter<Afspraak>();
-
-    dagnummer: number;
-    isToday = false;
-    isPeildatum = false;
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes['dag']) {
-            this.dagnummer = this.dag.getDate();
-            this.isToday = isSameDay(this.dag, new Date());
-        }
-        this.isPeildatum = isSameDay(this.dag, this.peildatum);
-    }
 }
